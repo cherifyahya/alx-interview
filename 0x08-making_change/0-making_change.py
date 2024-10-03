d903d09bdfc7573269c5a40a269642605c9ffad7
yahya <cherifyahya069@gmail.com>
2024-10-03 15:07:40 +0000

    solved

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
+""" 0-making_change.py """
+from typing import List
 
-""" Contains makeChange function"""
 
-
-def makeChange(coins, total):
+def makeChange(coins: List[int], total: int) -> int:
     """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
+    function: makeChange
+    args: coins -> List[int], total: int
     """
-    if not coins or coins is None:
-        return -1
     if total <= 0:
         return 0
-    change = 0
-    coins = sorted(coins)[::-1]
-    for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if (total == 0):
-            return change
-    return -1
+
+    sorted_coins = sorted(coins, reverse=True)
+    counter = 0
+    copy_total = total
+
+    for coin in sorted_coins:
+        if coin <= copy_total:
+            count = copy_total // coin
+            copy_total -= coin * count
+            counter += count
+        if copy_total == 0:
+            break
+
+    if copy_total != 0:
+        return -1
+
+    return counter
